ead478b0873aea92b5c5f4d1b9073c346e51d1aa
Chris Lewis <chrislewis64@gmail.com>
2019-04-29 12:35:46 +0100

    separating classes

--- movingObj.py
+++ movingObj.py
@@ -1,34 +1,39 @@
-# class for moving object
-# includes basic physics but not using vectors yet
+import pygame
+import pygame.math as pgm
 
 class movingObj:
 
-    def __init__(self):
-        self.x = 10
-        self.y = 20
-        self.speedx = 0.0
-        self.speedy = 0.0
-        self.accelx = 0.0
-        self.accely = 0.0
+    def __init__(self, x,y):
+        self.pos = pgm.Vector2(x,y)
+        self.velocity = pgm.Vector2(0,0)
+        self.acceleration = pgm.Vector2(0,0)
+        self.w = 20
+        self.h = 20
+        self.colour = 0
 
+    def checkBoundary(self):
+
+        if (self.pos.y  > screenH: 
+            self.pos.y = screenH
+	    self.velocity.y *= -1
+        
+        if self.pos.x < 0:
+            self.pos.x = 0
+            self.velocity.x *= -1
+        elif self.pos.x > screenX:
+            self.pos.x = screenW
+            self.velocity.x *= -1
+	 
     def update(self):
-	# acceleration changes velocity and velocity changes position
 		
-	# add acceleration
-	self.speedx += self.accelx
-        self.speedy += self.accely
-	
-	# update speed / velocity 
-        self.x += self.speedx
-        self.y += self.speedy
-	
-	# reset accel for next cycle
-        self.accelx = self.accely = 0.0
+        self.velocity += self.acceleration
+        self.pos += self.velocity
+        self.acceleration *= 0
+        self.checkBoundary()	    
 		
-    def applyForce(self, force) # force is tuple
-        self.accelx += force[0]
-        self.accely += force[1]
+    def applyForce(self, force): #forces accumulate before update 
+        self.acceleration += force
 		
     def show(self):
-	    pass
-	
+        pygame.draw.rect(screen, self.colour, [self.pos.x, self.pos.y, self.w, self.h], 1)
+
