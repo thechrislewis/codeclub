8f50b03944ce0113779630edbb67d654eadc2b38
Chris Lewis <chrislewis64@gmail.com>
2019-04-29 12:37:42 +0100

    separating classes into separate files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame.math as pgm
 from math import pi
+from movingObj import movingObj
 
 # Initialize the game engine
 pygame.init()
@@ -17,42 +18,6 @@ screenH = 480
 
 bounce = pgm.Vector2(1,-1)
 gravity = pgm.Vector2(0,0.2)
-
-class movingObj:
-
-    def __init__(self, x,y):
-        self.pos = pgm.Vector2(x,y)
-        self.velocity = pgm.Vector2(0,0)
-        self.acceleration = pgm.Vector2(0,0)
-        self.w = 20
-        self.h = 20
-        self.colour = 0
-
-    def checkBoundary(self):
-
-        if (self.pos.y  > screenH: 
-            self.pos.y = screenH
-	    self.velocity.y *= -1
-        
-        if self.pos.x < 0:
-            self.pos.x = 0
-            self.velocity.x *= -1
-        elif self.pos.x > screenX:
-            self.pos.x = screenW
-            self.velocity.x *= -1
-	 
-    def update(self):
-		
-        self.velocity += self.acceleration
-        self.pos += self.velocity
-        self.acceleration *= 0
-        self.checkBoundary()	    
-		
-    def applyForce(self, force): #forces accumulate before update 
-        self.acceleration += force
-		
-    def show(self):
-        pygame.draw.rect(screen, self.colour, [self.pos.x, self.pos.y, self.w, self.h], 1)
 
  
 # Set the height and width of the screen
